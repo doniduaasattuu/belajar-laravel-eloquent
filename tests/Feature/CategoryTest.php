4eb12b619e915b6e7d8916b223cc245af43edcc6
doniduaasattuu <doni.duaasattuu@gmail.com>
2023-09-15 13:53:31 +0700

    insert many

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -19,4 +19,33 @@ class CategoryTest extends TestCase
         $result = $category->save();
         self::assertTrue($result);
     }
+
+    public function testInsertMany()
+    {
+        $category = new Category();
+
+        $categories = [
+            [
+                "id" => "FOOD",
+                "name" => "Food"
+            ],
+            [
+                "id" => "GADGET",
+                "name" => "Gadget"
+            ]
+        ];
+
+        for ($i = 2; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "ID - $i",
+                "name" => "Name - $i",
+            ];
+        }
+
+        $result = $category->query()->insert($categories);
+        self::assertTrue($result);
+
+        $total = $category->query()->count();
+        self::assertEquals(10, $total);
+    }
 }
