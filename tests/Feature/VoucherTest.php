a530a5ac4f4c3da49e356b6ab47cb0eecd61ea37
Doni Darmawan <doni.duaasattuu@gmail.com>
2023-09-15 21:32:23 +0700

    soft deletes

--- tests/Feature/VoucherTest.php
+++ tests/Feature/VoucherTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Voucher;
+use Database\Seeders\VoucherSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Log;
@@ -39,5 +40,36 @@ class VoucherTest extends TestCase
         foreach ($vouchers as $voucher) {
             Log::info(json_encode($voucher, JSON_PRETTY_PRINT));
         }
+    }
+
+    public function testSoftDeletes()
+    {
+        $this->seed(VoucherSeeder::class);
+
+        $voucher = Voucher::where("name", "=", "Sample Voucher")->first();
+        self::assertNotNull($voucher);
+        $voucher->delete();
+
+
+        $voucher = Voucher::where("name", "=", "Sample Voucher")->first();
+        self::assertNull($voucher);
+    }
+
+    public function testSoftDeletesWithTrashed()
+    {
+        $this->seed(VoucherSeeder::class);
+
+        $voucher = Voucher::where("name", "=", "Sample Voucher")->first();
+        self::assertNotNull($voucher);
+        $voucher->delete();
+
+        // tidak menggunakan with trashed maka akan return null
+        $voucher = Voucher::where("name", "=", "Sample Voucher")->first();
+        self::assertNull($voucher);
+
+        // menggunakan withTrashed maka akan return record dengan deleted_at != null;
+        $voucher = Voucher::withTrashed()->where("name", "=", "Sample Voucher")->first();
+        self::assertNotNull($voucher);
+        self::assertEquals("Sample Voucher", $voucher->name);
     }
 }
